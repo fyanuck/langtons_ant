9dac4a170a024899ce3c18f1d83e24ed4793ad69
fyanuck <33692980+fyanuck@users.noreply.github.com>
2024-10-11 19:01:26 +0300

    Crude trace

--- worm.py
+++ worm.py
@@ -1,5 +1,6 @@
 import pygame
 import numpy as np
+from pprint import pprint
 
 
 pygame.init()
@@ -36,16 +37,43 @@ rot_dirs = {
 ant_direction = 'n'
 ant_coords = [width // 2, height // 2]
 
+# Ant's trace
+trace_max_len = 128  # Let the max len be 256
+trace_points = []
+trace_base_color = (255, 0, 0)  # RED
+trace_final_color = (255, 128, 128)   # The color of the last element of the trace
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREY = (128, 128, 128)
+
+trace_points_colors = []  # Starting with more intense, ending with white-ish
+
+
+R_diff = trace_final_color[0] - trace_base_color[0]
+G_diff = trace_final_color[1] - trace_base_color[1]
+B_diff = trace_final_color[2] - trace_base_color[2]
+
+# How much should each consecutive trace point color change
+R_step = R_diff / trace_max_len
+G_step = G_diff / trace_max_len
+B_step = B_diff / trace_max_len
+
+R_temp, G_temp, B_temp = trace_base_color
+for p in range(trace_max_len-1, -1, -1):
+        R_temp += R_step
+        G_temp += G_step
+        B_temp += B_step
+        trace_points_colors.append((int(R_temp), int(G_temp), int(B_temp)))
+
+
 # World
-# world = [[1 for x in range(width)] for y in range(height)]
 world = np.ones((width, height), dtype=int)
 
 # Screen
 screen = pygame.display.set_mode(window_size)
 pygame.display.set_caption(f"Langton's Ant: {width}x{height} cells")
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREY = (128, 128, 128)
+
 
 
 # Update
@@ -67,6 +95,15 @@ def update_world():
     # 5. (optional) Prevent out of bounds: %
     ant_coords[0] %= width
     ant_coords[1] %= height
+    # 6. Update trace
+    update_trace(ant_coords)
+
+
+# Update trace
+def update_trace(new_coords):
+    new_x, new_y = new_coords
+    global trace_points
+    trace_points = [(new_x, new_y)] + trace_points[:trace_max_len]
     
 
 # Draw
@@ -75,6 +112,13 @@ def draw_world(world):
         for y in range(height):
             if not world[x, y]:
                 pygame.draw.rect(screen, BLACK, (x * cell_size, y * cell_size, cell_size, cell_size))
+
+# Draw trace
+def draw_trace(world):
+    for trace_point, point_color in zip(trace_points, trace_points_colors):
+        x, y = trace_point
+        pygame.draw.rect(screen, point_color, (x * cell_size, y * cell_size, cell_size, cell_size))
+
 
 # Main loop
 running = True
@@ -93,6 +137,7 @@ while running:
     # Draw
     screen.fill(WHITE)
     draw_world(world)
+    draw_trace(world)
     pygame.display.flip()
 
     # Tick
